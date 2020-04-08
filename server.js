e9cc395742dfcc64daeb154b8ea507652fcdf304
blakelower <blakelower1999@gmail.com>
2020-04-08 15:04:30 -0700

    created the server

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require('express');
-
+const express = require("express");
+const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
 const server = express();
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
+server.use(express.json());
+server.use(logger);
+server.use("/posts", postRouter);
+server.use("/users", userRouter);
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log([`${req.method} Request to ${req.originalUrl} ${new Date().toISOString()}`]);
+
+  next();
+}
 
 module.exports = server;
